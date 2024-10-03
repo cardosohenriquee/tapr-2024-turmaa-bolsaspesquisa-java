47a8991d15964dd9a71b31d1e0f6cef24f4f3a10
cardosohenriquee <henlherme48@gmail.com>
2024-10-03 00:15:35 +0000

    first

---
+++ microservicegestaobolsistapesquisa/src/main/java/br/univille/microservicegestaobolsistapesquisa/MicroservicegestaobolsistapesquisaApplication.java
@@ -0,0 +1,13 @@
+package br.univille.microservicegestaobolsistapesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicegestaobolsistapesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicegestaobolsistapesquisaApplication.class, args);
+	}
+
+}
---
+++ microservicegestaobolsistapesquisa/src/test/java/br/univille/microservicegestaobolsistapesquisa/MicroservicegestaobolsistapesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.microservicegestaobolsistapesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicegestaobolsistapesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
